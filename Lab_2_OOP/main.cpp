d6608e623ad94360535ddd2a53f34279db5025dc
sl <nekomaroff@yandex.ru>
2025-12-08 18:58:44 +0300

    Initial Commit

---
+++ Lab_2_OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab_2_OOP
+//
+//  Created by Вячеслав Комаров on 08.12.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
